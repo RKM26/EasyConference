d47e131ac0b836df8a8e71e88c83cc3781c57e60
apurva <apuayush@gmail.com>
2019-12-23 14:25:07 +0530

    branch up

--- backend/controllers/main.py
+++ backend/controllers/main.py
@@ -4,7 +4,7 @@ from controllers.modules import *
 class AudioStreamHandler(RequestHandler):
     def set_default_headers(self):
         print("setting headers!!!")
-        # self.set_header("Content-Type", "application/json")
+        self.set_header("Content-Type", "application/json")
         self.set_header("Access-Control-Allow-Origin", "*")
         self.set_header('Access-Control-Allow-Methods', 'GET, OPTIONS')
 
